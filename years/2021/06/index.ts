249736814670c36932e67a1096a94893eb38f589
David Fourie <david.fourie@stonethree.com>
2021-12-06 08:55:19 +0200

    Add day 6

--- years/2021/06/index.ts
+++ years/2021/06/index.ts
@@ -12,17 +12,115 @@ const DAY = 6;
 // data path    : /Users/david/Workspace/advent-of-code/advent-of-code/years/2021/06/data.txt
 // problem url  : https://adventofcode.com/2021/day/6
 
+class Fish {
+	timer = 8;
+
+	constructor(timer?: number) {
+		if (timer !== undefined) {
+			this.timer = timer;
+		}
+	}
+
+	increment() {
+		// This is when a new fish gets created
+		if (this.timer === 0) {
+			this.timer = 6;
+			return new Fish();
+		} else {
+			this.timer--;
+			return undefined;
+		}
+	}
+}
+
+function calculate_fish(input: string, days: number) {
+	const initialState = input.split(",").map(value => Number(value.trim()));
+	// Get initial state
+	let school = initialState.map(value => new Fish(value));
+	let new_fish: Fish[] = [];
+
+	// log(`Initial state: ${initialState}`);
+	for (let i = 0; i < days; i++) {
+		new_fish = [];
+		// Increment each day
+		school.forEach(fish => {
+			let newFish = fish.increment();
+
+			if (newFish !== undefined) {
+				new_fish.push(newFish);
+			}
+		});
+		//Add new fish to school
+		school = [...school, ...new_fish];
+		// log(`After ${i + 1} day: ${school.map(fish => fish.timer)}`);
+	}
+
+	return school.length;
+}
+class TimerSchool {
+	school: Record<number, number> = {};
+
+	addFish(timer: number, numberOfFish: number) {
+		if (timer in this.school) {
+			this.school[timer] = this.school[timer] + numberOfFish;
+		} else {
+			this.school[timer] = numberOfFish;
+		}
+	}
+	computeNewSchool() {
+		const newSchool = new TimerSchool();
+		// Loop through timers
+		Object.keys(this.school).forEach(key => {
+			// Sillyness because of object.keys
+			const intKey = parseInt(key);
+			// This means that new fish are needed. We know how many to add
+			if (intKey === 0) {
+				// Add this amount of new fish
+				newSchool.addFish(8, this.school[intKey]);
+				// The new school also needs the old fish, but they start at 6
+				newSchool.addFish(6, this.school[intKey]);
+			} else {
+				// For every other class, simply add it into a new timer
+				newSchool.addFish(intKey - 1, this.school[intKey]);
+			}
+		});
+		return newSchool;
+	}
+	computerNumberOfFish() {
+		let total = 0;
+		Object.values(this.school).forEach(value => {
+			total += value;
+		});
+		return total;
+	}
+}
+
+function fastFishTransform(input: string, days: number) {
+	// Instead of naively keeping each fish, we group them together by their timers. This means that
+	// Complexity doesn't increase with time
+	let school = new TimerSchool();
+	const initialState = input.split(",").map(value => Number(value.trim()));
+	initialState.forEach(fishInitial => {
+		school.addFish(fishInitial, 1);
+	});
+	for (let i = 0; i < days; i++) {
+		school = school.computeNewSchool();
+	}
+
+	return school.computerNumberOfFish();
+}
+
 async function p2021day6_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	return calculate_fish(input, 80);
 }
 
 async function p2021day6_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	return fastFishTransform(input, 256);
 }
 
 async function run() {
 	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part2tests: TestCase[] = [{ input: "3,4,3,1,2", expected: "26984457539" }];
 
 	// Run tests
 	test.beginTests();
@@ -37,7 +135,6 @@ async function run() {
 		}
 	});
 	test.endTests();
-
 	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
 
@@ -45,7 +142,7 @@ async function run() {
 	const part1Solution = String(await p2021day6_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2021day6_part2(input));
 	const part2After = performance.now();
 
